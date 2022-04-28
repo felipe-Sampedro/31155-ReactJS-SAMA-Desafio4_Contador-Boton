148207b1e59e88dddfb84a306be67a628c78e5c3
felipe.sampedro <pipesancho182@gmail.com>
2022-04-28 17:05:18 -0500

    entrega terminada

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import React from 'react'
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar/NavBar'
+import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
+        <NavBar />
+        <ItemListContainer />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -18,6 +24,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Card from '../Card/Card'
+
+const ItemListContainer = ({greetings}) => {
+  return (
+    <div className='bg-info'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eius adipisci ipsum natus deserunt accusamus voluptate.
+
+        <p>{greetings}</p>
+        <div className="d-flex justify-content-evenly p-4">
+
+          <Card stock={5}/>
+          <Card stock={2}/>
+          <Card stock={8}/>
+
+        </div>
+
+    </div>
+  )
+}
+
+export default ItemListContainer
